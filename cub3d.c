bef88a0f0d1939bd1452903e05300efa97c4d949
nevaspid <romain.brendle.guido@gmail.com>
2023-12-28 19:50:28 +0100

    mlx_42_tests

--- cub3d.c
+++ cub3d.c
@@ -6,7 +6,7 @@
 /*   By: gloms <rbrendle@student.42mulhouse.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 18:41:39 by gloms             #+#    #+#             */
-/*   Updated: 2023/12/28 19:41:47 by gloms            ###   ########.fr       */
+/*   Updated: 2023/12/28 19:50:12 by gloms            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ int	main(void)
 	mlx_get_monitor_size(0, &display->width, &display->height);
 	printf("width : %d\n", (int)display->width);
 	printf("height: %d\n", (int)display->height);
-	//display->mlx = mlx_init(display->width, display->height, "cub3d", false);
-	//mlx_loop(display->mlx);
+	// display->mlx = mlx_init(25*64, 25*64, "cub3d", false);
+	// mlx_loop(display->mlx);
 }
