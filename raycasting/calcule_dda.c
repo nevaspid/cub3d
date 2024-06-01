f165b86ba86ec320e3be9fbc865dc4c36202ebe6
olivier349 <olivierliove@gmail.com>
2024-06-01 20:03:06 +0200

    [fix]variable not used

--- raycasting/calcule_dda.c
+++ raycasting/calcule_dda.c
@@ -6,7 +6,7 @@
 /*   By: oliove <oliove@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 00:46:50 by oliove            #+#    #+#             */
-/*   Updated: 2024/06/01 19:59:18 by oliove           ###   ########.fr       */
+/*   Updated: 2024/06/01 20:01:09 by oliove           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -195,7 +195,7 @@ void run_raycast(t_display *display, t_ray *ray, t_player *player)
     clear_image(display->raycast->ray->img, 0x000000);
     while (x < WIDTH)
     {
-        double angle_mini = player->angle - FOV / 2;
+        // double angle_mini = player->angle - FOV / 2;
         double angle_maxi = player->angle + (FOV) / 2;
         ray->angle = angle_maxi;//player->angle; //+ angle_rad(FOV) / 2 - x * angle_rad(FOV) / WIDTH;
         ray->dir.x = cos(player->angle);
