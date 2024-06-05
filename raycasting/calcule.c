95c6cf6ef1ec88f45c1d09cc653ed911fa61a057
olivier349 <olivierliove@gmail.com>
2024-06-05 02:53:22 +0200

    (save, update) start 3d render

--- raycasting/calcule.c
+++ raycasting/calcule.c
@@ -6,7 +6,7 @@
 /*   By: oliove <oliove@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 23:22:10 by oliove            #+#    #+#             */
-/*   Updated: 2024/06/05 02:36:23 by oliove           ###   ########.fr       */
+/*   Updated: 2024/06/05 02:52:45 by oliove           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,25 +23,26 @@ double distance(t_vec_d a, t_vec_d b)
 }
 
 
-void render_floor(t_display *d, t_ray *ray, t_player *player)
-{
-    int x;
-    int y;
-    int color;
-    
+// void render_floor(t_display *d, t_ray *ray, t_player *player)
+// {
+//     int x;
+//     int y;
+//     int color;
+//     (void)player;
+//     (void)ray;
 
-    while (y < HEIGHT)
-    {
-        y = HEIGHT / 2;
+//     while (y < HEIGHT)
+//     {
+//         y = HEIGHT / 2;
         
-        while (x < WIDTH)
-        {
-            color = 0x000000;
-            mlx_pixel_put(d->mlx, x, y, color);
-            x++;
-        }
-        y++;
-    }
+//         while (x < WIDTH)
+//         {
+//             color = 0x000000;
+//             mlx_pixel_put(d->mlx, x, y, color);
+//             x++;
+//         }
+//         y++;
+//     }
     
     
-}
+// }
