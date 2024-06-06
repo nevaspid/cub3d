c9f5b923c15ad0050e84ead1f86985e479f505e6
gloms <109989668+nevaspid@users.noreply.github.com>
2024-06-06 17:38:06 +0200

    proper way two fix the double player on map problem

--- minimap/print_minimap.c
+++ minimap/print_minimap.c
@@ -14,37 +14,41 @@
 
 void	print_assets(t_display *mlx, int tile_size, int x, int y)
 {
-	int	print_p;
-
-	print_p = 0;
 	if (mlx->m->minimap_array[y][x] == '1')
 		wall(mlx->m, x * tile_size, y * tile_size, tile_size);
 	else if (mlx->m->minimap_array[y][x] == '0')
 		floors(mlx->m, x * tile_size, y * tile_size, tile_size);
-	if (is_char(mlx->m->minimap_array[y][x], mlx) && print_p == 0)
+	else if (is_char(mlx->m->minimap_array[y][x], mlx))
 	{
 		mlx->m->p_y = y ;
 		mlx->m->p_x = x ;
 		mlx->p_y = y;
 		mlx->p_x = x;
 		mlx->m->player = mlx_new_image(mlx->mlx, tile_size / 2, tile_size / 2);
-		print_p = 1;
+		print_p++;
 	}
-	else
-		floors(mlx->m, x * tile_size, y * tile_size, tile_size);
 }
 
-void	print_minimap(t_display *mlx, t_minimap *minimap, int tile_size)
+void	print_minimap(t_display *mlx, t_minimap *minimap, int tile_size, t_mem_alloc *mylloc)
 {
 	int	x;
 	int	y;
+	int	print_p;
 
 	x = 0;
 	y = 0;
+	print_p = 0;
 	while (minimap->minimap_array[y])
 	{
 		while (minimap->minimap_array[y][x])
 		{
+			if (is_char(mlx->m->minimap_array[y][x], mlx))
+				print_p++
+			if (print_p > 1)
+			{
+				printf("ERROR : more than one player on map");
+				free_and_exit(mylloc);
+			}
 			print_assets(mlx, tile_size, x, y);
 			x++;
 		}
