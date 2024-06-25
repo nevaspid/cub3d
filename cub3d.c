bd503e8364c4a8da15c18b051b1da5ee56a307da
nevaspid <romain.brendle.guido@gmail.com>
2024-06-25 14:13:00 +0200

    Added define_tile_size in main

--- cub3d.c
+++ cub3d.c
@@ -3,14 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oliove <oliove@student.42.fr>              +#+  +:+       +#+        */
+/*   By: gloms <rbrendle@student.42mulhouse.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 18:41:39 by gloms             #+#    #+#             */
-/*   Updated: 2024/06/24 18:08:37 by oliove           ###   ########.fr       */
+/*   Updated: 2024/06/25 14:11:50 by gloms            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+void define_tile_size(t_display *display)
+{
+	if (longest_line(display->m->minimap_array) > count_lines(display->m->minimap_array))
+		display->m->tile_size = (WIDTH * SCALE) / longest_line(display->m->minimap_array);
+	else
+		display->m->tile_size = (HEIGHT * SCALE) / count_lines(display->m->minimap_array);
+}
 
 int	main(int ac, char **av)
 {
@@ -29,17 +37,14 @@ int	main(int ac, char **av)
 	display = mem_alloc(mylloc, sizeof(t_display));
 	init_game(display, mylloc);
 	read_parse_store(av[1], mylloc, display);
-	if (longest_line(display->m->minimap_array) > count_lines(display->m->minimap_array))
-		display->m->tile_size = (WIDTH * SCALE) / longest_line(display->m->minimap_array);
-	else
-		display->m->tile_size = (HEIGHT * SCALE) / count_lines(display->m->minimap_array);
+	define_tile_size(display);
 	print_minimap(display, display->m, display->m->tile_size, mylloc);
 	if (flood_fill(display->m->copy, display->m->p_x, display->m->p_y) > 0)
 	{
 		printf("ERROR : Map is not closed\n");
 		free_and_exit(mylloc);
 	}
-	
+
 	run_raycast(display,display->raycast->ray, display->raycast->player);
 	image_to_window(display);
 	mlx_loop_hook(display->mlx, &player_angle, display);
